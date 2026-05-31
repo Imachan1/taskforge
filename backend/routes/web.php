d8e3e0d258d38e40cc79d59c0634511fbd0e8ef4
Valeriia <hohunv@gmail.com>
2026-05-31 20:09:25 +0200

    fix: add /auth/google alias for oauth redirect

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -7,5 +7,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/auth/google', [GoogleAuthController::class, 'redirect']);
 Route::get('/auth/google/redirect', [GoogleAuthController::class, 'redirect']);
 Route::get('/auth/google/callback', [GoogleAuthController::class, 'callback']);
